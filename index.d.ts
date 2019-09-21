c3eef20531da9ebc2e3319670ca192aaaf396de3
ryan anzer <ryan.anzer@epitech.eu>
2019-09-22 01:42:45 +0200

    🚧 fix type

--- index.d.ts
+++ index.d.ts
@@ -3,7 +3,7 @@ declare class Util {
     tap(cd: (res: any) => Promise<any> | any): (res: any) => Promise<any> | any;
     timeout(cd: () => Promise<any> | any, n: number, err?: any): Promise<any>;
     delay(n?: number): Promise<any>;
-    measure(cd: (...any[]) => any): Promise<number>;
+    measure(cd: (...args: any[]) => any): Promise<number>;
     each(data: any, cd: (v: any, k: number | string) => any, max?: number): Promise<any>;
     bulk(data: any[], cd: (v: any, k: number | string) => any, max?: number, size?: number): Promise<any>;
 }
